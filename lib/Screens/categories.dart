99ebb31b02345ba03e4dbeb1469ad5980da104b1
Eslam Fathi <efathi2511@gmail.com>
2024-08-11 18:16:27 +0300

    Refactor CategoriesScreen to StatefulWidget with Animation and Enhance Meal Details and Meal Item Widgets
    - Convert CategoriesScreen from StatelessWidget to StatefulWidget to manage state and animations.
    - Introduce an AnimationController for smooth transitions in the CategoriesScreen.
    - Implement AnimatedBuilder and SlideTransition for animated category grid item appearance.
    - Add responsive grid layout for CategoriesScreen, adjusting the number of cross-axis items based on screen width.
    - Enhance MealDetailsScreen and MealItem widgets with Hero animations for smoother image transitions.
    - Ensure proper disposal of AnimationController to avoid memory leaks.
    - Update meal filtering logic to use widget's availableMeals list for category selection.

--- lib/Screens/categories.dart
+++ lib/Screens/categories.dart
@@ -1,22 +1,113 @@
 import 'package:flutter/material.dart';
+
 import 'package:meals_app/data/dummy_data.dart';
 import 'package:meals_app/models/category.dart';
 import 'package:meals_app/models/meal.dart';
 import 'package:meals_app/screens/meals.dart';
 import 'package:meals_app/widgets/category_grid_item.dart';
 
-class CategoriesScreen extends StatelessWidget {
+class CategoriesScreen extends StatefulWidget {
   const CategoriesScreen({
     super.key,
- 
     required this.availableMeals,
   });
 
-
   final List<Meal> availableMeals;
 
+  @override
+  State<CategoriesScreen> createState() => _CategoriesScreenState();
+}
+
+class _CategoriesScreenState extends State<CategoriesScreen>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _animationController;
+
+  @override
+  void initState() {
+    super.initState();
+    initAnimationMethod();
+  }
+
+  void initAnimationMethod() {
+    _animationController = AnimationController(
+      vsync: this,
+      duration: const Duration(milliseconds: 500),
+      lowerBound: 0,
+      upperBound: 1,
+    );
+    _animationController.forward();
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    _animationController.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return AnimatedBuilder(
+      animation: _animationController,
+      builder: (context, child) => SlideTransition(
+        position: Tween<Offset>(
+          begin: const Offset(0, .3),
+          end: const Offset(0, 0),
+        ).animate(
+          CurvedAnimation(
+            parent: _animationController,
+            curve: Easing.emphasizedDecelerate,
+          ),
+        ),
+        child: child,
+      ),
+      child: LayoutBuilder(
+        builder: (context, constraints) {
+          return constraints.maxWidth > 600
+              ? GridView(
+                  padding: const EdgeInsets.all(24),
+                  gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: 3,
+                    childAspectRatio: 3 / 2,
+                    crossAxisSpacing: 20,
+                    mainAxisSpacing: 20,
+                  ),
+                  children: [
+                    // availableCategories.map((category) => CategoryGridItem(category: category)).toList()
+                    for (final category in availableCategories)
+                      CategoryGridItem(
+                        category: category,
+                        onSelectCategory: () {
+                          _selectCategory(context, category);
+                        },
+                      )
+                  ],
+                )
+              : GridView(
+                  padding: const EdgeInsets.all(24),
+                  gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: 2,
+                    childAspectRatio: 3 / 2,
+                    crossAxisSpacing: 20,
+                    mainAxisSpacing: 20,
+                  ),
+                  children: [
+                    // availableCategories.map((category) => CategoryGridItem(category: category)).toList()
+                    for (final category in availableCategories)
+                      CategoryGridItem(
+                        category: category,
+                        onSelectCategory: () {
+                          _selectCategory(context, category);
+                        },
+                      )
+                  ],
+                );
+        },
+      ),
+    );
+  }
+
   void _selectCategory(BuildContext context, Category category) {
-    final filteredMeals = availableMeals
+    final filteredMeals = widget.availableMeals
         .where((meal) => meal.categories.contains(category.id))
         .toList();
 
@@ -25,57 +116,8 @@ class CategoriesScreen extends StatelessWidget {
         builder: (ctx) => MealsScreen(
           title: category.title,
           meals: filteredMeals,
- 
         ),
       ),
     ); // Navigator.push(context, route)
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return LayoutBuilder(
-
-      builder: (context, constraints) {
-        return constraints.maxWidth > 600 ? 
-        GridView(
-        padding: const EdgeInsets.all(24),
-        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-          crossAxisCount: 3,
-          childAspectRatio: 3 / 2,
-          crossAxisSpacing: 20,
-          mainAxisSpacing: 20,
-        ),
-        children: [
-          // availableCategories.map((category) => CategoryGridItem(category: category)).toList()
-          for (final category in availableCategories)
-            CategoryGridItem(
-              category: category,
-              onSelectCategory: () {
-                _selectCategory(context, category);
-              },
-            )
-        ],
-      ) : GridView(
-        padding: const EdgeInsets.all(24),
-        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-          crossAxisCount: 2,
-          childAspectRatio: 3 / 2,
-          crossAxisSpacing: 20,
-          mainAxisSpacing: 20,
-        ),
-        children: [
-          // availableCategories.map((category) => CategoryGridItem(category: category)).toList()
-          for (final category in availableCategories)
-            CategoryGridItem(
-              category: category,
-              onSelectCategory: () {
-                _selectCategory(context, category);
-              },
-            )
-        ],
-      );
-      },
-      
-    );
   }
 }
